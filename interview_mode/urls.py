cc067a7160eb280db53f2c3c5ba87c73a82a7e29
ViVi09030 <159188826+ViVi09030@users.noreply.github.com>
2025-10-17 10:22:23 -0500

    Update urls.py

--- interview_mode/urls.py
+++ interview_mode/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.interview, name='interview'),
+    path('<int:session_id>/', views.interview, name='interview_session'),
+    path('api/send-message/<int:session_id>/', views.send_message, name='send_message'),
+    path('api/create-session/', views.create_session, name='create_session'),
 ]
